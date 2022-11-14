f97a798d0d8733bdc7078667953996d9a73b74a5
adriancoloma <carlos.coloma@epn.edu.ec>
2022-11-14 17:56:54 -0500

    variables

---
+++ 01-javascript/01-variable.js
@@ -0,0 +1,15 @@
+const numero = 1;
+const sueldo = 1.2;
+const texto = "Adrian";
+const apellidos = "Eguez";
+const booleano = true;
+const hijos = null;
+const zapatos = 1;
+
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof apellidos);
+console.log(typeof booleano);
+console.log(typeof hijos);
+console.log(typeof zapatos);
